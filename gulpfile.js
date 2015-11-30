1bc8dc6bd0f362394f1c5947be9e6c6f0ab20fd6
Valentin LÉVÊQUE <contact@valentinleveque.me>
2015-11-30 16:52:48 +0100

    Lower case and dash spaced filenames

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,28 @@
-var gulp     = require('gulp');
+var gulp       = require('gulp');
 
-var del      = require('del');
-var pngcrush = require('imagemin-pngcrush');
-var path     = require('path');
-var imagemin = require('gulp-imagemin');
-var svg2png  = require('gulp-svg2png');
-var newer    = require('gulp-newer');
+var del        = require('del');
+var pngcrush   = require('imagemin-pngcrush');
+var path       = require('path');
+var imagemin   = require('gulp-imagemin');
+var svg2png    = require('gulp-svg2png');
+var newer      = require('gulp-newer');
+var exec       = require('child_process').exec;
+var rename     = require('gulp-rename');
+var changeCase = require('change-case');
 
-var project  = path.join(__dirname + '/');
-var ignore   = path.join('!' + __dirname + '/');
+var project    = path.join(__dirname + '/');
+var ignore     = path.join('!' + __dirname + '/');
+var dist       = project + 'dist/';
+var srcLogos   = project + 'src/**/*.svg';
+var distPngs   = project + 'dist/**/*.png';
 
 gulp.task('svgo', function () {
-    return gulp.src([
-        project + 'src/**/*.svg',
-    ])
-        .pipe(newer(project + 'dist/'))
+    return gulp.src(srcLogos)
+        .pipe(newer(dist))
+        .pipe(rename(function(path, file) {
+            path.basename = changeCase.paramCase(path.basename);
+            return path;
+        }))
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{
@@ -23,13 +31,22 @@ gulp.task('svgo', function () {
                 removeUselessStrokeAndFill: false
             }],
         }))
-        .pipe(gulp.dest(project + 'dist/'));
+        .pipe(gulp.dest(dist));
+});
+
+gulp.task('minify-png', function () {
+    return gulp.src(distPngs)
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngcrush()]
+        }))
+        .pipe(gulp.dest(dist));
 });
 
 gulp.task('svg2png', function () {
-    gulp.src(project + 'src/**/*.svg')
+    gulp.src(srcLogos)
         .pipe(svg2png())
-        .pipe(gulp.dest(project + 'dist/'));
+        .pipe(gulp.dest(dist));
 });
 
 gulp.task('default', ['svgo', 'svg2png']);
