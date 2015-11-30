e9c15163def5373e3e9dc4b12264c26afcc036b6
Valentin LÉVÊQUE <contact@valentinleveque.me>
2015-11-30 15:34:48 +0100

    Create gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp         = require('gulp');
+
+var del          = require('del');
+var pngcrush     = require('imagemin-pngcrush');
+var path         = require('path');
+var imagemin     = require('gulp-imagemin');
+var svg2png      = require('gulp-svg2png');
+
+var project      = path.join(__dirname + '/');
+var ignore       = path.join('!' + __dirname + '/');
+
+gulp.task('svgo', function () {
+    return gulp.src([
+        project + 'src/**/*.svg',
+    ])
+        .pipe(newer(project + 'dist/'))
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{
+                removeViewBox: false
+            }, {
+                removeUselessStrokeAndFill: false
+            }],
+        }))
+        .pipe(gulp.dest(project + 'dist/'));
+});
+
+gulp.task('svg2png', function () {
+    gulp.src(project + 'src/**/*.svg')
+        .pipe(svg2png())
+        .pipe(gulp.dest(project + 'dist/'));
+});
+
+gulp.task('default', ['svgo', 'svg2png']);
